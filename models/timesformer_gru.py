17c7273ffb239757afc518878dbc36bee3823360
K0ntact <duyan5745@gmail.com>
2024-12-24 09:18:19 +0700

    update

--- models/timesformer_gru.py
+++ models/timesformer_gru.py
@@ -9,13 +9,29 @@ class TimesformerGRU(nn.Module):
         super().__init__(*args, **kwargs)
         self.processor = AutoImageProcessor.from_pretrained(pretrained_tsf)
         self.timesformer = TimesformerModel.from_pretrained(pretrained_tsf)
-        self.gru = nn.GRU(input_size=768, hidden_size=gru_hidden_size, num_layers=gru_layers, batch_first=True)
+        self.gru = nn.GRU(input_size=768, hidden_size=gru_hidden_size, num_layers=gru_layers, batch_first=False)
         self.classifier = nn.Linear(gru_hidden_size, num_classes)
 
+    def __batched_tensor_to_list(self, batched_tensor: torch.Tensor):
+        """
+        Convert batched tensor of videos to list of videos, where each video is a list of frames, each frame is a tensor
+        """
+        list_tensor = []
+        for vid_tensor in batched_tensor.unbind(0):
+            list_tensor.append(vid_tensor.unbind(0))
+        return list_tensor
+
     def forward(self, x, *args, **kwargs):
-        inputs = self.processor(images=x, return_tensors="pt")
+        # x shape: batch, frames, channels, height, width
+        x_device = x.device
+        x_list = self.__batched_tensor_to_list(x)
+
+        # Processor only takes in list of tensors
+        inputs = self.processor(images=x_list, return_tensors="pt")
+        inputs.to(x_device)
+
         with torch.no_grad():
             outputs = self.timesformer(**inputs)
-            cls_reps = outputs[0][:, 0, :]
+            cls_reps = outputs[0][:, 0, :]  # batch, hidden_size
         _, hidden = self.gru(cls_reps.unsqueeze(0))
         return self.classifier(hidden[-1])
